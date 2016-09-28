509a7b2f17e12f5e70b05704529bdee3fe0ea718
antonsbox <antonsbox2010@gmail.com>
2016-09-28 15:27:27 +0300

    simple http server test added

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import * as http from "http";
 module HttpServerTest {
 
     class App {
-
+        httpServer: any;
 
         constructor() {
-
+            this.httpServer = http.createServer(this.onRequest).listen(8888);
 
         }
+
+        onRequest(req, res) {
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.write("Hello World!!");
+            res.end();
+        }
     }
+    var mainApp = new App();
 }
